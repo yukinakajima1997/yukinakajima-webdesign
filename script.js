6192e6e367b67c23a151de1d9d02e15094457da9
yukinakajima1997 <70610054+yukinakajima1997@users.noreply.github.com>
2021-01-22 10:53:18 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+$(function(){
+  var pagetop = $('#page_top');
+  pagetop.hide();
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
+});
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
